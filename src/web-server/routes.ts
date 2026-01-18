c8387fbe9eae542d4687d0406d0c1a0b92f93445
Tal Arbetov <talarbatov98@gmail.com>
2026-01-18 18:31:46 +0200

    added all routes

--- src/web-server/routes.ts
+++ src/web-server/routes.ts
@@ -1,7 +1,12 @@
 const routes = {
     heartbeat: '/heartbeat',
     accounts: {
-        create: '/api/accounts'
+        create: '/api/accounts',
+        balance: '/api/accounts/:accountId/balance',
+        deposit: '/api/accounts/:accountId/deposit',
+        withdraw: '/api/accounts/:accountId/withdraw',
+        block: '/api/accounts/:accountId/block',
+        statement: '/api/accounts/:accountId/statement',
     }
 }
 
